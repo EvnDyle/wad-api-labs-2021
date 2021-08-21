325c28a97cd1b87a960f26cab8177481b0187489
EvnDyle <20085432@mail.wit.com>
2021-08-21 05:29:47 +0100

    Session based auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,8 @@ import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
 import {loadUsers} from './seedData';
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
 
 
 
@@ -31,10 +33,17 @@ if (process.env.SEED_DB) {
 //configure body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 app.use(express.static('public'));
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
+
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
 
 
 app.listen(port, () => {
