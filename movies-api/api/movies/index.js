de2de68680e727b792249d6fa029459aca073108
EvnDyle <20085432@mail.wit.com>
2021-08-21 19:45:55 +0100

    Passport

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import {
-  getMovies, getMovie, getMovieReviews, getGenres
+  getMovieReviews, getGenres
 } from '../tmdb-api';
 import movieModel from './movieModel';
 
